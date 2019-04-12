848d9042148f7a214ecd25ec717ef5ce77132fd9
DarrenB8 <darrenbanh8@gmail.com>
2019-04-11 20:20:13 -0600

    Update Button.java

--- Button.java
+++ Button.java
@@ -13,6 +13,12 @@ import javax.swing.*;
  * Class used to set up buttons. Set up the text of a button, color of the button and interface. 
  */
 public class Button extends JButton {
+    
+    
+    /**
+     * Constructor that is used to set up the buttons.
+     * @param text (Type String) - What you want to the text of the button to be.
+     */
     public Button(String text) {
         super(text);
         setBackground(Configure.Theme.BTN_COLOR);
