a0d1618248c4581a410c9eb68257220368fb5a9d
DarrenB8 <darrenbanh8@gmail.com>
2019-04-11 18:08:32 -0600

    Update Button.java

--- Button.java
+++ Button.java
@@ -9,6 +9,9 @@ import com.game.common.Configure;
 
 import javax.swing.*;
 
+/**
+ * Class used to set up buttons. Set up the text of a button, color of the button and interface. 
+ */
 public class Button extends JButton {
     public Button(String text) {
         super(text);
